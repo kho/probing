9b1a9cb0b1b1df958f1ce6dade6b802632e8733b
Wu, Ke <wuke@cs.umd.edu>
2014-04-30 22:56:05 -0400

    New default hash function from fasthash's mix

--- probing_params.go
+++ probing_params.go
@@ -6,17 +6,12 @@ type __Value uint64
 const __KEY_NIL = ^__Key(0)
 
 func __hash(k __Key) uint {
-	// hash64shift from
-	// https://web.archive.org/web/20120903003157/http://www.cris.com/~Ttwang/tech/inthash.htm.
-	r := uint64(k) // make sure >> is logical.
-	r = (^r) + (r << 21)
-	r = r ^ (r >> 24)
-	r = (r + (r << 3)) + (r << 8)
-	r = r ^ (r >> 14)
-	r = (r + (r << 2)) + (r << 4)
-	r = r ^ (r >> 28)
-	r = r + (r << 31)
-	return uint(r)
+	// https://code.google.com/p/fast-hash
+	h := uint64(k)
+	h ^= h >> 23
+	h *= 0x2127599bf4325c37
+	h ^= h >> 47
+	return uint(h)
 }
 
 func __equal(a, b __Key) bool {
